67cfa6136d23f0c2fa228967cf7c5475a47b5be3
nebularaid <sharmarkewarsama18@gmail.com>
2023-08-19 16:43:53 +0100

    1.1

--- src/Books.java
+++ src/Books.java
@@ -1,15 +1,14 @@
 
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Books {
-    private ArrayList<Book> toRead = new ArrayList();
-    private ArrayList<Book> currentRead = new ArrayList();
-    private ArrayList<Book> read = new ArrayList();
+    private final ArrayList<Book> toRead = new ArrayList<>();
+    private final ArrayList<Book> currentRead = new ArrayList<>();
+    private final ArrayList<Book> read = new ArrayList<>();
     private int booksRead;
     private int booksToRead;
 
@@ -17,76 +16,67 @@ public class Books {
     }
 
     public void setBooks(String file) {
-        try {
-            Scanner reader = new Scanner(Paths.get(file));
+        try (Scanner reader = new Scanner(Paths.get(file))) {
 
-            try {
-                reader.nextLine();
-                Pattern pattern = Pattern.compile("\"([^\"]*)\"");
+            reader.nextLine();
+            Pattern titleOrReview = Pattern.compile("\"([^\"]*)\"");
+            Pattern dateReadPat = Pattern.compile("^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$");
 
-                while(reader.hasNextLine()) {
-                    String line = reader.nextLine();
-                    Matcher matcher = pattern.matcher(line);
-                    String title = null;
-                    String review = null;
-                    if (matcher.find()) {
-                        title = matcher.group();
-                    }
-
-                    if (matcher.find()) {
-                        review = matcher.group();
-                        line.replaceAll("\"([^\"]*)\"", "NULL");
-                    }
-
-                    String[] columns = line.split(",");
-                    if (columns[0].equals("NULL")) {
-                        columns[0] = title;
-                    }
-
-                    if (columns.length > 6) {
-                        columns[7] = review;
-                    }
-
-                    int rating = Integer.valueOf(columns[2]);
-                    double avgRating = Double.valueOf(columns[3]);
-                    int pages = Integer.valueOf(columns[4]);
-                    int published = Integer.valueOf(columns[5]);
-                    Book book = new Book();
-                    book.setBook(columns[0], columns[1], rating, avgRating, pages, published);
-                    if (!review.equals((Object)null)) {
-                        book.setReview(review);
-                    } else if (!columns[7].equals("")) {
-                        book.setReview(columns[7]);
-                    }
-
-                    String shelf = columns[6];
-                    if (shelf.equals("to-read")) {
-                        this.addToTBR(book);
-                        ++this.booksToRead;
-                    } else if (shelf.equals("read")) {
-                        this.addRead(book);
-                        ++this.booksRead;
-                    } else if (shelf.equals("currently-reading")) {
-                        this.addCurrentRead(book);
-                    }
-                }
-            } catch (Throwable var17) {
-                try {
-                    reader.close();
-                } catch (Throwable var16) {
-                    var17.addSuppressed(var16);
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                Matcher matcherToR = titleOrReview.matcher(line);
+                Matcher matcherDR = dateReadPat.matcher(line);
+                String title = null;
+                String review = null;
+                String dateRead = null;
+                if (matcherToR.find()) {
+                    title = matcherToR.group();
                 }
 
-                throw var17;
+                if (matcherToR.find()) {
+                    review = matcherToR.group();
+                    line.replaceAll("\"([^\"]*)\"", "NULL");
+                }
+
+                if (matcherDR.find()) dateRead = matcherDR.group();
+
+                String[] columns = line.split(",");
+                if (columns[0].equals("NULL")) columns[0] = title;
+
+                int last = columns.length - 1;
+                if (!columns[last].equals("to-read|read|currently-reading") && review != null) {
+                    columns[last] = review;
+                } else if (!columns[last].equals("to-read|read|currently-reading")) {
+                    review = columns[last];
+                }
+
+                // refactor tip: maybe replace empty rows with
+                // a space so columns.length is always the same
+
+                int rating = Integer.parseInt(columns[2]);
+                double avgRating = Double.parseDouble(columns[3]);
+                int pages = Integer.parseInt(columns[4]);
+                int published = Integer.parseInt(columns[5]);
+                Book book = new Book();
+                book.setBook(columns[0], columns[1], rating, avgRating, published, pages);
+                if (dateRead != null) book.setDateRead(dateRead);
+
+                if (line.contains("to-read")) {
+                    this.addToTBR(book);
+                    ++this.booksToRead;
+                } else if (line.contains("read")) {
+                    this.addRead(book);
+                    ++this.booksRead;
+                } else if (line.contains("currently-reading")) {
+                    this.addCurrentRead(book);
+                }
             }
-
-            reader.close();
-        } catch (Exception var18) {
-            System.out.println("--ERROR--");
-            var18.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("ERROR");
+            e.printStackTrace();
         }
-
     }
+
 
     public void addToTBR(Book book) {
         this.toRead.add(book);
@@ -101,35 +91,23 @@ public class Books {
     }
 
     public void getCurrentRead() {
-        Iterator var1 = this.currentRead.iterator();
-
-        while(var1.hasNext()) {
-            Book book = (Book)var1.next();
+        for (Book book : this.currentRead) {
             System.out.println(book);
         }
-
         System.out.println();
     }
 
     public void getRead() {
-        Iterator var1 = this.read.iterator();
-
-        while(var1.hasNext()) {
-            Book book = (Book)var1.next();
+        for (Book book : this.read) {
             System.out.println(book);
         }
-
         System.out.println();
     }
 
     public void getTBR() {
-        Iterator var1 = this.toRead.iterator();
-
-        while(var1.hasNext()) {
-            Book book = (Book)var1.next();
+        for (Book book : toRead) {
             System.out.println(book);
         }
-
         System.out.println();
     }
 }
